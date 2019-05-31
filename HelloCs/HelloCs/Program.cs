00ecd4d94e23d01cfb698d93f90c8ebe4f51d7a3
hellojohnwoo <hellojohnwoo@gmail.com>
2019-05-31 20:19:00 +0900

    0531 Arguments C#-study

---
+++ HelloCs/HelloCs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloCs
+{
+    class MainClass
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write(args.Length); // HelloCs test1 test2 -> args : 2
+
+            // Console.WriteLine("Hello " + args[0]); // no args -> error -> just build!
+
+
+            Console.ReadKey();
+        }
+    }
+}
